eb8045073ee05f60d864d681840fda8f661f627d
Juliang Li <Juliang@JuliangMacBookPro.local>
2015-11-21 17:10:48 -0600

    Update define function to be more user-friendly

--- js/commands.js
+++ js/commands.js
@@ -13,13 +13,21 @@ exports.define = function(number, word){
     needle.get("https://api.pearson.com:443/v2/dictionaries/ldoce5/entries?headword=" + word, null,
               function(error,response,body){
                     var results = "";
-                    for (var i = 0,j = 0; i < body.results.length; ++i){
-                        if (body.results[i].headword.toLowerCase() === word.toLowerCase()){
-                            results += ((++j) + ": " + body.results[i].senses[0].definition[0] + ".\n");
+                    var definitions = [];
+                    for (var i = 0; i < body.results.length; ++i){
+                    if (body.results[i].headword.toLowerCase() === word.toLowerCase()){
+                        definitions.push(body.results[i].senses[0].definition[0]);
                         }
                     }
-                    if (results === "")
+                    if (definitions.length === 0){
                         results = "Word(s) not defined.";
+                    }else if(definitions.length === 1) {
+                        results = definitions[0] + ".\n";
+                    }else{
+                        for (var i = 0; i < definitions.length; ++i){
+                            results += (i+1) + ": " + definitions[i] + ".\n";
+                        }
+                    }
                     twilio.sendMessage(number, results);
                });
     
