5ba136610fb5fb9d05bd309b5ebe7883f982e31a
Sahil Dhanju <sahildhanju1@gmail.com>
2015-11-21 14:17:44 -0600

    yep

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require("express");
-
 var app = express();
 
 app.get("/", function(req, res)
@@ -7,9 +6,7 @@ app.get("/", function(req, res)
 	res.send("Hello world!");
 });
 
-app.set('port', (process.env.port || 3000));
-
-app.listen(app.get('port'), function()
-{
-	console.log('Server listening on port ' + app.get('port'));
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
